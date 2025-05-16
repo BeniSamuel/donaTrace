9a784c938233325521a556a165c656215b5d12f0
benisamuel <benisamuel566@gmail.com>
2025-05-16 19:54:11 +0200

    added controller on user service

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,43 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Put } from "@nestjs/common";
+import { UserService } from "./users.service";
+import { ResponseEntity } from "src/utils/response.util";
+import { User } from "src/model/user.model";
+import { RegisterDto } from "src/dto/register.dto";
 
-@Controller()
+@Controller("/api/users")
 export class UserController {
+    constructor (private readonly userService: UserService) {}
 
+    @Get("/all")
+    @HttpCode(HttpStatus.OK)
+    async getAllUsers (): Promise<ResponseEntity<User[]>> {
+        return ResponseEntity.ok("Successfully obtained all users!!! 🎉🎉🎉", await this.userService.getAllUsers())
+    }
+
+    @Get("/:userId")
+    @HttpCode(HttpStatus.OK | HttpStatus.NOT_FOUND)
+    async getUserById (@Param("userId") userId: number): Promise<ResponseEntity<User>> {
+        const user = await this.userService.getUserById(userId);
+        if (user != null) {
+            return  ResponseEntity.ok("Successfully obtained user!!! 🎉🎉🎉", user);
+        }
+        return ResponseEntity.notFound("Sorry user not found!!! 😔💔💔", null);
+    }
+
+    @Put("/:userId")
+    @HttpCode(HttpStatus.OK | HttpStatus.NOT_FOUND)
+    async updateUserById (@Param("userId") userId: number, @Body() registerDto: RegisterDto): Promise<ResponseEntity<User>> {
+        const user = await this.userService.updateUserById(userId, registerDto);
+        if (user != null) {
+            return ResponseEntity.ok("Successfully updated user!!! 🎉🎉🎉", user);
+        }
+        return ResponseEntity.notFound("Sorry user not found!!! 😔💔💔", null);
+    }
+
+    @Delete("/:userId")
+    async deleteUserById (@Param("userId") userId: number): Promise<ResponseEntity<Boolean>> {
+        return this.userService.deleteUserById(userId) ?
+        ResponseEntity.ok("Successfully deleted user!!! 🎉🎉🎉", true) :
+        ResponseEntity.notFound("Sorry user not found!!! 😔💔💔", null); 
+    }
 }
