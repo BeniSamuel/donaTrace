776ad67cab0ef31827441edb58faeaccc568517f
benisamuel <benisamuel566@gmail.com>
2025-05-16 19:54:42 +0200

    added typeorm configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { User } from "./model/user.model";
+import { Donation } from "./model/donation.model";
+import { Inventory } from "./model/inventory.model";
+import { Hospital } from "./model/hospital.model";
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: "localhost",
+      port: 5432,
+      username: "postgres",
+      password: "beni@ish",
+      database: "donatrace",
+      entities: [User, Donation, Inventory, Hospital],
+      synchronize: true
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
