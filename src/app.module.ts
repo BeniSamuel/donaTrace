c9692be5fd34b364f54cdd9c4c89f30d66004ee7
benisamuel <benisamuel566@gmail.com>
2025-05-16 22:52:19 +0200

    Improved app module to support modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,15 @@ import { User } from "./model/user.model";
 import { Donation } from "./model/donation.model";
 import { Inventory } from "./model/inventory.model";
 import { Hospital } from "./model/hospital.model";
+import { UserModule } from "./users/users.module";
+import { JwtModule } from "@nestjs/jwt";
 
 @Module({
   imports: [
+    JwtModule.register({
+      global: true,
+      secret: "&4!0!BeNi@Ish#123*$!!"
+    }),
     TypeOrmModule.forRoot({
       type: "postgres",
       host: "localhost",
@@ -18,7 +24,8 @@ import { Hospital } from "./model/hospital.model";
       database: "donatrace",
       entities: [User, Donation, Inventory, Hospital],
       synchronize: true
-    })
+    }),
+    UserModule
   ],
   controllers: [AppController],
   providers: [AppService],
