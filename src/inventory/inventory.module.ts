fcaa389f913956721ecae1845509757e794e5893
benisamuel <benisamuel566@gmail.com>
2025-05-17 11:53:13 +0200

    imported inventory service

--- src/inventory/inventory.module.ts
+++ src/inventory/inventory.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { Inventory } from "src/model/inventory.model";
 import { InventoryController } from "./inventory.controller";
 import { InventoryService } from "./inventory.service";
+import { HospitalModule } from "src/hospital/hospital.module";
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Inventory])],
+    imports: [TypeOrmModule.forFeature([Inventory]), HospitalModule],
     controllers: [InventoryController],
     providers: [InventoryService],
 })
