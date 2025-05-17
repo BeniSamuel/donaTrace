6c235d7a851cfdbb4013c305a01794bd71e1de48
benisamuel <benisamuel566@gmail.com>
2025-05-17 11:14:48 +0200

    Added inventory service , module and controller

--- src/inventory/inventory.controller.ts
+++ src/inventory/inventory.controller.ts
@@ -1,9 +1,36 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post } from "@nestjs/common";
 import { InventoryService } from "./inventory.service";
+import { ResponseEntity } from "src/utils/response.util";
+import { Inventory } from "src/model/inventory.model";
+import { InventoryDto } from "src/dto/inventory.dto";
 
 @Controller("/api/donatrace/inventory")
 export class InventoryController {
     constructor (private readonly inventoryService: InventoryService) {};
 
+    @Get("/all")
+    @HttpCode(HttpStatus.OK)
+    async getAllInventory (): Promise<ResponseEntity<Inventory[]>> {
+        return ResponseEntity.ok("Successfully obtained all inventory!!! 🎉🎉🎉", await this.inventoryService.getAllInventories());
+    }
+
+    @Get("/hospital/hospitalId")
+    @HttpCode(HttpStatus.OK || HttpStatus.NOT_FOUND)
+    async getAllInventoryByHospitalId (@Param("hospitalId") hospitalId: number): Promise<ResponseEntity<Inventory[]>> {
+        const inventories: Inventory[] = await this.inventoryService.getInventoriesByHospital(hospitalId);
+        if (inventories.length !== 0) {
+            return ResponseEntity.ok("Successfully obtained inventories from hospital!!! 🎉🎉🎉", inventories);
+        }
+        return ResponseEntity.notFound("Sorry hospital not found!!! 😔💔💔", null);
+    }
     
+    @Post("/create")
+    @HttpCode(HttpStatus.CREATED || HttpStatus.NOT_FOUND)
+    async createInventory (@Body() inventoryDto: InventoryDto): Promise<ResponseEntity<Inventory>> {
+        const newInventory = await this.inventoryService.createInventory(inventoryDto);
+        if (newInventory != null) {
+            return ResponseEntity.created("Successfully created an inventory!!! 🎉🎉🎉", newInventory);
+        }
+        return ResponseEntity.notFound("Sorry hospital not found!!! 😔💔💔", null);
+    }
 }
--- src/inventory/inventory.service.ts
+++ src/inventory/inventory.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
+import { InventoryDto } from "src/dto/inventory.dto";
+import { HospitalService } from "src/hospital/hospital.service";
 import { Inventory } from "src/model/inventory.model";
 import { Repository } from "typeorm";
 
 @Injectable()
 export class InventoryService {
-    constructor (@InjectRepository(Inventory) private readonly inventoryRepository: Repository<Inventory>) {};
+    constructor (
+        @InjectRepository(Inventory) private readonly inventoryRepository: Repository<Inventory>,
+        private readonly hospitalService: HospitalService
+    ) {};
 
     getAllInventories (): Promise<Inventory[]> {
         return this.inventoryRepository.find();
@@ -15,4 +20,34 @@ export class InventoryService {
         return this.inventoryRepository.findOne({where: {id}});
     }
     
+    async getInventoriesByHospital (id: number): Promise<Inventory[]> {
+        const hospital = await this.hospitalService.getHospitalById(id);
+        if (hospital != null) {
+            return this.inventoryRepository.find({where: {hospital}});
+        }
+        return null;
+    }
+
+    async createInventory (inventoryDto: InventoryDto): Promise<Inventory> {
+        const hospital = await this.hospitalService.getHospitalById(inventoryDto.hospital_id);
+        if (hospital != null) {
+            const newInventory = new Inventory(hospital, inventoryDto.bloodType);
+            return this.inventoryRepository.save(newInventory);
+        }
+        return null;
+    }
+    
+    async deleteInventory (hospitalId: number, inventoryId: number): Promise<Boolean> {
+        const hospital = await this.hospitalService.getHospitalById(hospitalId);
+        if (hospital == null) return false;
+
+        const inventory = await this.inventoryRepository.findOne({where: {
+            id: inventoryId,
+            hospital: hospital
+        }})
+        if (inventory == null) return false;
+
+        this.inventoryRepository.delete(inventory);
+        return true;
+    }
 }
